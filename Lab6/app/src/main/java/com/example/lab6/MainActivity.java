ad8bb82333408f8bef96fb7a6f5bdb69a45d0e5e
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-07-06 18:17:58 +0530

    Lab 6 Completed

--- Lab6/app/src/main/java/com/example/lab6/MainActivity.java
+++ Lab6/app/src/main/java/com/example/lab6/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.lab6;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText username,password;
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,30 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        username=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+        login=findViewById(R.id.button);
+
+        String trueUsername="admin";
+        String truePassword="abc123";
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user=username.getText().toString();
+                String pass=password.getText().toString();
+                if (user.equals(trueUsername) && pass.equals(truePassword)){
+                    Toast t = Toast.makeText(getApplicationContext(),"Login Successful",Toast.LENGTH_LONG);
+                    t.show();
+                }
+                else{
+                    Toast t = Toast.makeText(getApplicationContext(),"Login Failed",Toast.LENGTH_LONG);
+                    t.show();
+                }
+            }
+        });
+
+
     }
 }

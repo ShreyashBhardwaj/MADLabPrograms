f641209924bbe902cc46cc8d8f81753800fc8477
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-09-02 14:58:28 +0530

    Ok I understand what is happening

---
+++ Program8/app/src/main/java/com/example/program8/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.program8;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        loadFragment(new FirstFragment());
+    }
+    public void loadFirstFragment(View view) {
+        loadFragment(new FirstFragment());
+    }
+
+    // Called by android:onClick="loadSecondFragment"
+    public void loadSecondFragment(View view) {
+        loadFragment(new SecondFragment());
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fragment_container, fragment);
+        ft.commit();
+    }
+
+}

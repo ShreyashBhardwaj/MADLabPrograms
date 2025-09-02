850b0fe015f1e74309f951fb87a7d7c4c536ef16
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-09-02 13:20:59 +0530

    Lab 5 Done now to check how to send data

--- LabProgram5/app/src/main/java/com/example/labprogram5/MainActivity.java
+++ LabProgram5/app/src/main/java/com/example/labprogram5/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.labprogram5;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button toImplicit,toExplicit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        toImplicit = findViewById(R.id.Implicit);
+        toExplicit = findViewById(R.id.Explicit);
+
+        toExplicit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
+            }
+        });
+        toImplicit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https:google.com"));
+                startActivity(i);
+            }
+        });
     }
+
+
+
 }

dcff6e7f08f5e57e22da70c180eb7e26852f90cf
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-09-02 15:39:16 +0530

    Added Program 9

--- Program8/app/src/main/java/com/example/program8/SecondFragment.java
+++ Program8/app/src/main/java/com/example/program8/SecondFragment.java
@@ -2,6 +2,9 @@ package com.example.program8;
 
 import android.os.Bundle;
 import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,13 +14,20 @@ public class SecondFragment extends Fragment {
 
     public SecondFragment() {}
 
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        Log.d("FragmentLifecycle", "SecondFragment - onCreateView");
+//        return inflater.inflate(R.layout.fragment_second, container, false);
+//    }
+
+
+    
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container,
                              Bundle savedInstanceState) {
-        Log.d("FragmentLifecycle", "SecondFragment - onCreateView");
+        Log.d("FragmentLifecycle","SecondFragment");
         return inflater.inflate(R.layout.fragment_second, container, false);
     }
 }
 
-
-// Now to do it Myself
---
+++ Program9/app/src/main/java/com/example/program9/ReceiverFragment.java
@@ -0,0 +1,41 @@
+package com.example.program9;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class ReceiverFragment extends Fragment {
+
+
+    public ReceiverFragment() {
+        // Required empty public constructor
+    }
+
+    TextView textView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_receiver, container, false);
+        textView = view.findViewById(R.id.tvResult);
+        return view;
+    }
+
+    public void updateText(String data) {
+        if (textView != null) {
+            textView.setText(data);
+        }
+    }
+
+
+}
